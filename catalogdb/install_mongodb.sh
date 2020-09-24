1566f313dd3fc6e898266f4fa1100d00c7d9c400
Simonbelete <simonbelete@gmail.com>
2020-09-24 02:03:20 +0000

    catalog database deployment

--- catalogdb/install_mongodb.sh
+++ catalogdb/install_mongodb.sh
@@ -4,4 +4,4 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 
 #helm repo update
 
-helm install --namespace guya-ltd catalog --version 9.1.2 bitnami/mongodb --values values.yaml
+helm install --namespace guya-ltd catalog bitnami/mongodb --values values.yaml
