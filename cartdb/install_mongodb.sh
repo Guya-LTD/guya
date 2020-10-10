86d6af9eefc220b3fa1941d0df86f94c3eef1892
Simonbelete <simonbelete@gmail.com>
2020-10-11 00:58:51 +0300

    Removing cart db deployment

--- cartdb/install_mongodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-#helm repo update
-
-helm install --namespace guya-ltd cartdb --version 9.1.2 bitnami/mongodb --values values.yaml
